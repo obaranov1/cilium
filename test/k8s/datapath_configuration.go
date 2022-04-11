f32101117ffc48a25a5abe1e24a1542d38dcdf2c
Martynas Pumputis <m@lambda.lt>
2022-04-11 18:44:52 +0200

    test: Enable node encryption for WG tests with vxlan
    Currently, the bpf_overlay prog doesn't redirect a packet to the WG
    netdev for encryption (will be addressed in a follow-up PR). So, in
    order for the tests to pass, we need to enable the host2host encryption.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8s/datapath_configuration.go
+++ test/k8s/datapath_configuration.go
@@ -452,7 +452,7 @@ var _ = Describe("K8sDatapathConfig", func() {
 		})
 	})
 
-	SkipContextIf(helpers.DoesNotRunOnNetNextKernel, "Wireguard encryption", func() {
+	SkipContextIf(helpers.DoesNotRunOnNetNextKernel, "WireGuard encryption", func() {
 		testWireguard := func(interNodeDev string) {
 			randomNamespace := deploymentManager.DeployRandomNamespaceShared(DemoDaemonSet)
 			deploymentManager.WaitUntilReady()
@@ -563,9 +563,10 @@ var _ = Describe("K8sDatapathConfig", func() {
 
 		It("Pod2pod is encrypted in tunneling mode", func() {
 			deploymentManager.DeployCilium(map[string]string{
-				"tunnel":             "vxlan",
-				"encryption.enabled": "true",
-				"encryption.type":    "wireguard",
+				"tunnel":                    "vxlan",
+				"encryption.enabled":        "true",
+				"encryption.type":           "wireguard",
+				"encryption.nodeEncryption": "true",
 			}, DeployCiliumOptionsAndDNS)
 
 			testWireguard("cilium_vxlan")
@@ -573,10 +574,11 @@ var _ = Describe("K8sDatapathConfig", func() {
 
 		It("Pod2pod is encrypted in tunneling mode with per-endpoint routes", func() {
 			deploymentManager.DeployCilium(map[string]string{
-				"tunnel":                 "vxlan",
-				"endpointRoutes.enabled": "true",
-				"encryption.enabled":     "true",
-				"encryption.type":        "wireguard",
+				"tunnel":                    "vxlan",
+				"endpointRoutes.enabled":    "true",
+				"encryption.enabled":        "true",
+				"encryption.type":           "wireguard",
+				"encryption.nodeEncryption": "true",
 			}, DeployCiliumOptionsAndDNS)
 
 			testWireguard("cilium_vxlan")
